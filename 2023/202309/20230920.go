5ce062167d6114124887d0214d401468fae588fb
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-09-20 17:16:34 +0800

    20230920每日一题

---
+++ 2023/202309/20230920.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+题目一:
+func main() {
+	var a int8 = -1
+	var b int8 = -128 / a
+
+	println(b)
+}
+
+题目二:
+func main() {
+    const a int8 = -1
+    var b int8 = -128 / a
+
+    println(b)
+}
+
+问他们分别输出什么？
+*/
+
+//答案以及解析:https://qgfki2dkcj.feishu.cn/docx/GpFidty4woLup7xj5d7cT8iPnth
