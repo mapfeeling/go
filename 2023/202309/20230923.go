2ac90336b6f62d2f62516d714c312c33c9fe7f0c
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-09-23 23:53:41 +0800

    每日一题

---
+++ 2023/202309/20230923.go
@@ -0,0 +1,27 @@
+package main
+
+// 下面这段代码能否编译通过？如果可以，输出什么？
+
+func GetValue() int {
+	return 1
+}
+
+func main() {
+	i := GetValue()
+	switch i.(type) {
+	case int:
+		println("int")
+	case string:
+		println("string")
+	case interface{}:
+		println("interface")
+	default:
+		println("unknown")
+	}
+}
+
+/*
+编译失败
+考点：类型断言，类型断言的语法形如：i.(type)，其中 i 是接口，type 是固定关键字
+需要注意的是，只有接口类型才可以使用类型断言
+*/
