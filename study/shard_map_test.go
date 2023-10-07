a1a1510c35da3cafb0b6f27a6bbdc3a607d3f87b
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-10-07 18:43:29 +0800

    update

---
+++ study/shard_map_test.go
@@ -0,0 +1,11 @@
+package study_test
+
+import (
+	"testing"
+	shard_map "github.com/mapfeeling/shard"
+	_ "github.com/mapfeeling/shard"
+)
+
+func BenchmarkItems(b *testing.B) {
+	var m := shard_map.New(string)
+}
