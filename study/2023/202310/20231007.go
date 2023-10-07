deeb170092470141448b92871c8969defc9793b6
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-10-07 18:10:04 +0800

    每日一题

---
+++ study/2023/202310/20231007.go
@@ -0,0 +1,17 @@
+package _02310
+
+import "fmt"
+
+func main() {
+	s := make(map[string]int)
+	delete(s, "h")
+	fmt.Println(s["h"])
+}
+
+// 下面这段代码输出什么？
+/*
+A. runtime panic
+B. 0
+C. compilation error
+*/
+// 删除 map 不存在的键值对时，不会报错，相当于没有任何作用；获取不存在的减值对时，返回值类型对应的零值，所以返回 0
