d8e45ca4f548f7eb9c90a5323ebfc85911619002
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-10-15 23:23:40 +0800

    go-study

---
+++ study/2023/202310/20231015_study_test.go
@@ -0,0 +1,19 @@
+package _02310
+
+import "fmt"
+
+func incr(p *int) int {
+	*p++
+	return *p
+}
+
+func main() {
+	p := 1
+	incr(&p)
+	fmt.Println(p)
+}
+
+// 已上输出什么？
+// 2
+// 指针，incr() 函数里的 p 是 *int 类型的指针，指向的是 main() 函数的变量 p 的地址
+// 第 2 行代码是将该地址的值执行一个自增操作，incr() 返回自增后的结果
