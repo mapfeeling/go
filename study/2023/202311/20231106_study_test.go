8791439969dc1d044075bde7334752729309a83f
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-11-06 10:11:14 +0800

    go

--- study/2023/202311/20231106_study_test.go
+++ study/2023/202311/20231106_study_test.go
@@ -31,6 +31,11 @@ func fThis() *SThis {
 }
 
 func TestStudy20231106(t *testing.T) {
-	p := fThis()     //B
-	fmt.Println(p.m) //print "foo"
+	p := fThis()     // B  *f() 或者 f()
+	fmt.Println(p.m) // print "foo"
 }
+
+/*
+f() 函数返回参数是指针类型，所以可以用 & 取结构体的指针
+B 处,如果填 *f(),则 p 是 S 类型；如果填 f(),则 p 是 *S 类型,不过都可以使用 p.m 取得结构体的成员
+*/
