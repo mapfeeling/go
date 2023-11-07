c7bd3e59e647ffec9dddb164aa278f805a2c865b
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-11-07 15:18:15 +0800

    go&rust

--- leet_code/2023/202311/20231107_medium_vowel_strings_test.go
+++ leet_code/2023/202311/20231107_medium_vowel_strings_test.go
@@ -38,6 +38,7 @@ func vowelStringsAnother(words []string, left int, right int) int {
 	return ans
 }
 
+// 统计范围内的元音字符串数
 func TestVowelStrings(t *testing.T) {
 	words := []string{"hey", "aeo", "mu", "ooo", "artro"}
 	words2 := []string{"are", "amy", "u"}
@@ -45,3 +46,9 @@ func TestVowelStrings(t *testing.T) {
 	fmt.Println(vowelStrings(words, left, right))
 	fmt.Println(vowelStringsAnother(words2, 0, 2))
 }
+
+/*
+给你一个下标从 0 开始的字符串数组 words 和两个整数：left 和 right
+如果字符串以元音字母开头并以元音字母结尾，那么该字符串就是一个 元音字符串 ,其中元音字母是 'a'、'e'、'i'、'o'、'u'
+返回 words[i] 是元音字符串的数目,其中 i 在闭区间 [left, right] 内
+*/
