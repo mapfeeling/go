aedcebde2db3df14a36c8c734951b570b43438a4
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-11-09 10:25:34 +0800

    go&rust

---
+++ study/2023/202311/20231109_study_test.go
@@ -0,0 +1,26 @@
+package _02311
+
+import (
+	"fmt"
+	"testing"
+)
+
+var a bool = true
+
+// return 之后的 defer 语句会执行吗，下面这段代码输出什么？
+func TestStudy20231109(t *testing.T) {
+	defer func() {
+		fmt.Println("1")
+	}()
+	if a == true {
+		fmt.Println("2")
+		return
+	}
+	defer func() {
+		fmt.Println("3")
+	}()
+}
+
+// defer 关键字后面的函数或者方法想要执行必须先注册
+// return 之后的 defer 是不能注册的
+// 也就不能执行后面的函数或方法
