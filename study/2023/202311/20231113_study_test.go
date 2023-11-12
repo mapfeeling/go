0448f677f9781f2fd9bf9b3bb789413d1ee2ad52
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-11-13 01:09:33 +0800

    go

---
+++ study/2023/202311/20231113_study_test.go
@@ -0,0 +1,24 @@
+package _02311
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 以下代码输出什么？
+func TestStudy20231113(t *testing.T) {
+	m := map[int]string{0: "zero", 1: "one"}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
+
+/*
+1 one
+0 zero
+*/
+// 或者
+/*
+0 zero
+1 one
+*/
